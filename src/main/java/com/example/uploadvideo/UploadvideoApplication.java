3825c057808dc80b78aab51f1d7af60e30d6a3f9
Huang Weixiang <mf20330034@smail.nju.edu.cn>
2021-06-12 23:20:36 +0800

    first “”

---
+++ src/main/java/com/example/uploadvideo/UploadvideoApplication.java
@@ -0,0 +1,13 @@
+package com.example.uploadvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadvideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadvideoApplication.class, args);
+	}
+
+}
